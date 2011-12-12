5d80705fda41829d988605d0fc6bca997e73b732
s8950 <wojniemczyk@gmail.com>
2011-12-12 21:03:19 +0100

    class Film_DirectorDBManager is modyfied

--- MPR2/src/main/java/project/RentalMain.java
+++ MPR2/src/main/java/project/RentalMain.java
@@ -1,4 +1,5 @@
 package project;
+
 import java.util.ArrayList;
 
 import java.util.List;
@@ -12,49 +13,43 @@ import services.FilmDBManager;
 import services.Film_DirectorDBManager;
 import statuses.FilmStatus;
 
-
-
 import events.FilmEvent;
 import events.FilmListener;
 import exceptions.EndOfShelfException;
 import exceptions.FilmNotFoundException;
 import exceptions.LocationIsNullException;
 
+public class RentalMain implements FilmListener {
 
-
-public class Rental implements FilmListener{
-
-	private static Logger logger = Logger.getLogger(Rental.class);
+	private static Logger logger = Logger.getLogger(RentalMain.class);
 	public Shelf shelf;
-	
-	
-	public Rental(Shelf shelf){
-		this.shelf = shelf;	
+
+	public RentalMain(Shelf shelf) {
+		this.shelf = shelf;
 	}
-	
-	
-	public Rental(){
+
+	public RentalMain() {
 		super();
 	}
-	
 
 	public static void main(String[] args) {
 
 		PropertyConfigurator.configure("Log4J.properties");
 
-		Rental rental = new Rental();
+		RentalMain rental = new RentalMain();
 		Shelf shelf = new Shelf();
 		shelf.addFilmListener(rental);
 		shelf.init();
-		
+
 		shelf.show();
-		
+
 		try {
-			shelf.putInLocation(new Location(5, 0), new Film("Bracia", "Susanne Bier", 2004, FilmStatus.Available));
+			shelf.putInLocation(new Location(5, 0), new Film("Bracia",
+					"Susanne Bier", 2004, FilmStatus.Available));
 		} catch (LocationIsNullException e) {
 			logger.warn(e.toString());
 		}
-		
+
 		shelf.show();
 
 		try {
@@ -82,13 +77,13 @@ public class Rental implements FilmListener{
 		}
 
 		try {
-			logger.info(shelf.changeLocation(new Location(2, 0), new Location(3, 0)));
+			logger.info(shelf.changeLocation(new Location(2, 0), new Location(
+					3, 0)));
 		} catch (LocationIsNullException e) {
 			logger.warn(e.toString());
 		}
-		
+
 		shelf.show();
-		
 
 		try {
 			Film f = shelf.findByLocation(new Location(1, 0));
@@ -98,31 +93,34 @@ public class Rental implements FilmListener{
 		} catch (LocationIsNullException e) {
 			logger.warn(e.toString());
 		}
-		
 
 		ArrayList<Film> newFilms = new ArrayList<Film>();
-		newFilms.add(new Film("Chinatown", "Roman Polański", 1974, FilmStatus.Available));
-		newFilms.add(new Film("Taxi Driver", "Martin Scorsese", 1976, FilmStatus.Available));
-		newFilms.add(new Film("Snatch", "Guy Ritchie", 2000, FilmStatus.Available));
-		newFilms.add(new Film("Nóż w wodzie", "Roman Polański", 1961, FilmStatus.Available));
-	
-		
-		
+		newFilms.add(new Film("Chinatown", "Roman Polański", 1974,
+				FilmStatus.Available));
+		newFilms.add(new Film("Taxi Driver", "Martin Scorsese", 1976,
+				FilmStatus.Available));
+		newFilms.add(new Film("Snatch", "Guy Ritchie", 2000,
+				FilmStatus.Available));
+		newFilms.add(new Film("Nóż w wodzie", "Roman Polański", 1961,
+				FilmStatus.Available));
+
 		try {
 			shelf.put(newFilms);
 		} catch (EndOfShelfException e) {
 			logger.warn(e.toString());
 		}
-		
+
 		shelf.show();
 
 		try {
 			try {
 				try {
-					logger.info(shelf.insertNewFilmOnOccupiedLocation("La Comunidad", new Location(0, 0), new Film("Mulholland Drive",
-							"David Lynch", 2002, FilmStatus.Available)));
+					logger.info(shelf.insertNewFilmOnOccupiedLocation(
+							"La Comunidad", new Location(0, 0), new Film(
+									"Mulholland Drive", "David Lynch", 2002,
+									FilmStatus.Available)));
 				} catch (EndOfShelfException e) {
-					
+
 					logger.info(e.toString());
 				}
 			} catch (LocationIsNullException e) {
@@ -131,64 +129,65 @@ public class Rental implements FilmListener{
 		} catch (FilmNotFoundException e) {
 			logger.warn(e.toString());
 		}
-		
+
 		shelf.show();
-		
+
 		shelf.removeByLocation(new Location(1, 0));
-		
+
 		shelf.show();
-		
+
 		try {
-			Film film = shelf.findByLocation(new Location(1,1));
+			Film film = shelf.findByLocation(new Location(1, 1));
 			shelf.reserve(film);
 		} catch (LocationIsNullException e) {
 			logger.warn(e.toString());
 		}
-	
-		
+
 		shelf.clearAll();
 		shelf.show();
-		
+
 		List<Film> films = new ArrayList<Film>();
-		films.add(new Film("Chinatown", "Roman Polański", 1974, FilmStatus.Available));
-		films.add(new Film("Taxi Driver", "Martin Scorsese", 1976, FilmStatus.Available));
+		films.add(new Film("Chinatown", "Roman Polański", 1974,
+				FilmStatus.Available));
+		films.add(new Film("Taxi Driver", "Martin Scorsese", 1976,
+				FilmStatus.Available));
 		films.add(new Film("Snatch", "Guy Ritchie", 2000, FilmStatus.Available));
-		films.add(new Film("Nóż w wodzie", "Roman Polański", 1961, FilmStatus.Available));
-	
+		films.add(new Film("Nóż w wodzie", "Roman Polański", 1961,
+				FilmStatus.Available));
+
 		FilmDBManager filmDBManager = new FilmDBManager();
 		filmDBManager.addListOfFilms(films);
-	
+
 		List<Director> directors = new ArrayList<Director>();
 		directors.add(new Director("Roman", "Polański", "France", 1933));
 		directors.add(new Director("Martin", "Scorsese", "USA", 1942));
 		directors.add(new Director("Guy", "Ritchie", "Great Britain", 1968));
 		directors.add(new Director("Susanne", "Bier", "Denmark", 1960));
 		directors.add(new Director("Alex", "de la Iglesia", "Spain", 1965));
-		
+
 		DirectorDBManager directorDBManager = new DirectorDBManager();
 		directorDBManager.addListOfDirectors(directors);
-		
+
+		/*
 		Film_DirectorDBManager film_directorDBManager = new Film_DirectorDBManager();
-		film_directorDBManager.addFilm_Director(new Film("Chinatown", "Roman Polański", 1974, FilmStatus.Available), new Director("Roman", "Polański", "France", 1933));
-		
+		film_directorDBManager.addFilm_Director(new Film("Chinatown",
+				"Roman Polański", 1974, FilmStatus.Available), new Director(
+				"Roman", "Polański", "France", 1933));
+		*/
 		
 		int i = filmDBManager.getIdFilmByTitle("Snatch");
 
 		System.out.println("\nId szukane: " + i);
-		
-		filmDBManager.deleteFilm(i);
-		
-		System.out.println(films);
-	
-		
-		int y = 1961;
-		System.out.println("\nId filmu z roku: " + y + " to: " + filmDBManager.getIdFilmByYear(y));
-		
-		
-		
-		
-	}
 
+		filmDBManager.deleteFilm(i);
+
+		System.out.println(films);
+
+		int y = 1961;
+		System.out.println("\nId filmu z roku: " + y + " to: "
+				+ filmDBManager.getIdFilmByYear(y));
+
+	}
 
 	
 	
@@ -198,8 +197,8 @@ public class Rental implements FilmListener{
 	
 	public void filmBorrowed(FilmEvent event) {
 		event.getFilm().setStatus(FilmStatus.Available);
-		System.out.println("Borrowed film: " + event.getFilm().getTitle());		
-		
+		System.out.println("Borrowed film: " + event.getFilm().getTitle());
+
 	}
 
 	public void filmReturned(FilmEvent event) {
@@ -207,13 +206,11 @@ public class Rental implements FilmListener{
 		System.out.println("Returned film: " + event.getFilm().getTitle());
 	}
 
-
 	public void filmReserved(FilmEvent event) {
 		event.getFilm().setStatus(FilmStatus.Reserved);
 		System.out.println("Reserved film: " + event.getFilm().getTitle());
 	}
 
-	
 	public void filmAdded(FilmEvent event) {
 		event.getFilm().setStatus(FilmStatus.Available);
 		System.out.println("Added film: " + event.getFilm().getTitle());
--- MPR2/src/main/java/services/Film_DirectorDBManager.java
+++ MPR2/src/main/java/services/Film_DirectorDBManager.java
@@ -1,4 +1,5 @@
 package services;
+
 import java.sql.*;
 import java.util.*;
 import java.sql.ResultSet;
@@ -8,134 +9,135 @@ import project.Film;
 import project.Film_Director;
 
 public class Film_DirectorDBManager {
-			
-			private Connection connection;
-			private Statement statement;
-			private PreparedStatement addFilm_DirectorStatement;
-			private PreparedStatement getFilm_DirectorStatement;
-			private PreparedStatement deleteFilm_DirectorStatement;
-			private PreparedStatement deleteAllFilm_DirectorsStatement;
+
+	private Connection connection;
+	private Statement statement;
+	private PreparedStatement addFilm_DirectorStatement;
+	private PreparedStatement getFilm_DirectorStatement;
+	private PreparedStatement deleteFilm_DirectorStatement;
+	private PreparedStatement deleteAllFilm_DirectorsStatement;
+
+	public Film_DirectorDBManager() {
+
+		try {
+
+			connection = DriverManager
+					.getConnection("jdbc:hsqldb:hsql://localhost/workdb");
+
+			statement = connection.createStatement();
+			boolean Film_DirectorTableExists = false;
+
+			ResultSet rs = connection.getMetaData().getTables(null, null, null,
+					null);
+
+			while (rs.next()) {
+				if ("Film_Director"
+						.equalsIgnoreCase(rs.getString("TABLE_NAME"))) {
+					Film_DirectorTableExists = true;
+					break;
+				}
+			}
+
+			if (!Film_DirectorTableExists) {
+				statement
+						.executeUpdate("CREATE TABLE Film_Director(id_film int, id_director int, CONSTRAIN id_film_fk FOREIGN KEY id_film REFERENCES film(id), CONSTRAIN id_director_fk FOREIGN KEY id_director REFERENCES director(id)");
+
+			}
 		
-			
-			public Film_DirectorDBManager() {
+			addFilm_DirectorStatement = connection
+					.prepareStatement("INSERT INTO Film_Director (id_film, id_director) VALUES (?, ?)");
 
-				try {
+			getFilm_DirectorStatement = connection
+					.prepareStatement("SELECT id_film, id_director FROM Film_Director");
 
-					connection = DriverManager
-							.getConnection("jdbc:hsqldb:hsql://localhost/workdb");
+			deleteFilm_DirectorStatement = connection
+					.prepareStatement("DELETE FROM Film_Director WHERE id_film = ? AND id_director = ?");
 
-					statement = connection.createStatement();
-					boolean Film_DirectorTableExists = false;
-
-					ResultSet rs = connection.getMetaData().getTables(null, null, null,
-							null);
-
-					while (rs.next()) {
-						if ("Film_Director".equalsIgnoreCase(rs.getString("TABLE_NAME"))) {
-							Film_DirectorTableExists = true;
-							break;
-						}
-					}
-
-					if (!Film_DirectorTableExists) {
-						statement
-								.executeUpdate("CREATE TABLE Film_Director(id_film INTEGER IDENTITY PRIMARY KEY, id_director Id INTEGER IDENTITY PRIMARY KEY)");
-					}
-
-					addFilm_DirectorStatement = connection
-							.prepareStatement("INSERT INTO Film_Director (id_film, id_director) VALUES (?, ?)");
-
-					getFilm_DirectorStatement = connection
-							.prepareStatement("SELECT id_film, id_director FROM Film_Director");
-				
-					deleteFilm_DirectorStatement = connection
-							.prepareStatement("DELETE FROM Film_Director WHERE id_film = ? AND id_director = ?");		
-					
-					deleteAllFilm_DirectorsStatement = connection
-							.prepareStatement("DELETE FROM Film_Director");		
-					
-				}
-
-				catch (SQLException e) {
-
-					e.printStackTrace();
-
-				}
-			}
-
-			public void addFilm_Director(Film f, Director d) {
-				
-				try {
-
-					FilmDBManager filmDBManager = new FilmDBManager();
-					DirectorDBManager directorDBManager = new DirectorDBManager();
-					
-					addFilm_DirectorStatement.setInt(1, filmDBManager.getIdFilmByTitle(f.getTitle()));
-					addFilm_DirectorStatement.setInt(2, directorDBManager.getIdDirectorBySurname(d.getSurname())); 
-					addFilm_DirectorStatement.executeUpdate();
-				
-				}
-
-				catch (SQLException e) {
-
-					e.printStackTrace();
-				}
-
-			}
-			
-			public List<Film_Director> getAllFilm_Directors() {
-
-				List<Film_Director> film_directors = new ArrayList<Film_Director>();
-
-				try {
-
-					ResultSet rs = getFilm_DirectorStatement.executeQuery();
-
-					while (rs.next()) {
-						
-						film_directors.add(new Film_Director(rs.getInt("id_film"), rs
-								.getInt("id_director")));
-					}
-					
-					return film_directors;
-
-				}
-
-				catch (SQLException e) {
-
-					e.printStackTrace();
-				}
-
-				return null;
-			}
-
-			public void deleteFilm_Director(int id_f, int id_d){
-				
-				try {
-					
-					deleteFilm_DirectorStatement.setInt(1, id_f);
-					deleteFilm_DirectorStatement.setInt(2, id_d);
-					deleteFilm_DirectorStatement.executeUpdate();
-				
-				} 
-				
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			
-			public void deleteAllFilm_Directors() {
-
-				try {
-
-					deleteAllFilm_DirectorsStatement.executeUpdate();
-				}
-
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
+			deleteAllFilm_DirectorsStatement = connection
+					.prepareStatement("DELETE FROM Film_Director");
 
 		}
 
+		catch (SQLException e) {
 
+			e.printStackTrace();
+
+		}
+	}
+
+	public void addFilm_Director(Film f, Director d) {
+
+		try {
+
+			FilmDBManager filmDBManager = new FilmDBManager();
+			DirectorDBManager directorDBManager = new DirectorDBManager();
+
+			addFilm_DirectorStatement.setInt(1,
+					filmDBManager.getIdFilmByTitle(f.getTitle()));
+			addFilm_DirectorStatement.setInt(2,
+					directorDBManager.getIdDirectorBySurname(d.getSurname()));
+			addFilm_DirectorStatement.executeUpdate();
+
+		}
+
+		catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public List<Film_Director> getAllFilm_Directors() {
+
+		List<Film_Director> film_directors = new ArrayList<Film_Director>();
+
+		try {
+
+			ResultSet rs = getFilm_DirectorStatement.executeQuery();
+
+			while (rs.next()) {
+
+				film_directors.add(new Film_Director(rs.getInt("id_film"), rs
+						.getInt("id_director")));
+			}
+
+			return film_directors;
+
+		}
+
+		catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public void deleteFilm_Director(int id_f, int id_d) {
+
+		try {
+
+			deleteFilm_DirectorStatement.setInt(1, id_f);
+			deleteFilm_DirectorStatement.setInt(2, id_d);
+			deleteFilm_DirectorStatement.executeUpdate();
+
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deleteAllFilm_Directors() {
+
+		try {
+
+			deleteAllFilm_DirectorsStatement.executeUpdate();
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
